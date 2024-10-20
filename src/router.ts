562a4dd5d6c02948ed1a5ac1ba3c816344bcd2fc
wolecodes <opeyemioyeyemi141@gmail.com>
2024-10-20 14:02:48 +0100

    feat(product): add product route

---
+++ src/router.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+const router = Router();
+
+//Create product route
+
+router.get("/product", () => {});
+router.get("/product/:id", () => {});
+
+router.post("/product", () => {});
+router.put("/product/:id", () => {});
+router.delete("/product/:id", () => {});
